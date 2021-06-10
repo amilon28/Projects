18729278f88a656bf90f483afc3f84f7e9b1bd2d
Amin <39567999+amilon28@users.noreply.github.com>
2021-06-11 00:33:11 +0430

    Update app.js

--- coronaVirus/app.js
+++ coronaVirus/app.js
@@ -4,8 +4,10 @@
 const numberOfConfirmed = document.querySelector(".number-confirmed");
 const numberOfRecovered = document.querySelector(".number-recovered");
 const numberOfDeath = document.querySelector(".number-death");
+const numberOfVaccin = document.querySelector(".number-vaccin");
 const countryFlag = document.querySelector(".flag");
 const deathRate = document.querySelector(".number-mortal");
+const vaccinRate = document.querySelector(".number-vaccin-rate");
 const loadingImg = document.querySelector(".loading");
 const card = document.querySelector(".card");
 const flag = document.querySelector(".country-flag");
@@ -24,6 +26,45 @@ const getFlag = function (country) {
     });
 };
 
+// for number of vaccinate people and vaccinated rate
+const vaccin = (country) => {
+  fetch("https://covid-api.mmediagroup.fr/v1/vaccines")
+    .then((res) => res.json())
+    .then((data) => {
+      const countryLists = Object.entries(data);
+      countryLists.forEach(([countryName, countryInfo]) => {
+        if (country === countryName) {
+          numberOfVaccin.innerText = countryInfo.All.people_vaccinated;
+          vaccinRate.innerText =
+            (
+              (countryInfo.All.people_vaccinated / countryInfo.All.population) *
+              100
+            ).toFixed(2) + " %";
+        } else {
+          return;
+        }
+      });
+    });
+};
+
+// death rate
+const mortalRate = (c) => {
+  fetch("https://covid-api.mmediagroup.fr/v1/cases")
+    .then((res) => res.json())
+    .then((data) => {
+      const countryList = Object.entries(data);
+      countryList.forEach(([countryName, countryInfo]) => {
+        if (c === countryName) {
+          deathRate.innerText =
+            (
+              (countryInfo.All.deaths / countryInfo.All.population) *
+              100
+            ).toFixed(4) + " %";
+        }
+      });
+    });
+};
+
 const getInfo = function (country) {
   fetch(`https://covid-19-data.p.rapidapi.com/country?name=${country}`, {
     method: "GET",
@@ -34,16 +75,12 @@ const getInfo = function (country) {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       getFlag(data[0].country);
       numberOfConfirmed.innerText = data[0].confirmed;
       numberOfRecovered.innerText = data[0].recovered;
       numberOfDeath.innerText = data[0].deaths;
-      deathRate.innerText = (
-        (data[0].deaths / data[0].confirmed) *
-        100
-      ).toFixed(2);
-      getFlag(data[0].country);
+      mortalRate(data[0].country);
+      vaccin(data[0].country);
     })
     .catch((err) => {
       console.error(err);
