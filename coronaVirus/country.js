7a335181cfe68fb37e70382407ad6086fcfe893b
Amin <39567999+amilon28@users.noreply.github.com>
2021-06-12 17:46:17 +0430

    Update country.js

--- coronaVirus/country.js
+++ coronaVirus/country.js
@@ -1,3 +1,10 @@
+const countryName = document.getElementById("country");
+const searchBtn = document.querySelector(".search-btn");
+
 document.querySelector(".search-btn").addEventListener("click", function () {
+  if (!countryName.value) {
+    alert("Select a country first!");
+    searchBtn.href = "country.html";
+  }
   localStorage.setItem("country", document.getElementById("country").value);
 });
