5f801af3c4cad0ef6b4b775a709303b4c28c127b
Amin Gharibshah <amingharibshah@gmail.com>
2021-07-17 05:23:54 +0430

    design through out net-ninja course

---
+++ public/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("#menu");
+
+burger.addEventListener("click", function () {
+  menu.classList.toggle("hidden");
+});
